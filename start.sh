024ad2b922e2e84a0ee05e9b94cfa7a252e84dd4
cednore <cednore@gmail.com>
2022-07-29 01:44:59 -0700

    ⚡ set working status when startup

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# cd inside script directory
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd $SCRIPT_DIR
 
+# show main window
 gjs index.js &
--- unlockmonitor.sh
+++ unlockmonitor.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# cd inside script directory
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd $SCRIPT_DIR
 
+# set working status when first run
+node update_custom_status.js "computer" "Working..."
+
+# watch for screen unlock event and update status
 dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
   while read x; do
     case "$x" in
