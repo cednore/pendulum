55896989af15adeac6d0ee36f342ab1b398c75e8
cednore <cednore@gmail.com>
2022-07-28 17:38:31 -0700

    ✨ unlock monitor

---
+++ unlockmonitor.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+cd $SCRIPT_DIR
+
+dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
+  while read x; do
+    case "$x" in
+      *"boolean false"*) node update_custom_status.js "house_with_garden" "Working...";
+    esac
+  done
