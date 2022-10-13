769deb495c016bb8dda3e6f14e13090507e20597
cednore <cednore@gmail.com>
2022-10-13 06:35:46 -0700

    ⚡ read default status emoji and text from env var

--- unlockmonitor.sh
+++ unlockmonitor.sh
@@ -4,13 +4,16 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd $SCRIPT_DIR
 
+PENDULUM_STATUS_EMOJI="${PENDULUM_STATUS_EMOJI:-'computer'}"
+PENDULUM_STATUS_TEXT="${PENDULUM_STATUS_TEXT:-'Working...'}"
+
 # set working status when first run
-node update_custom_status.js "computer" "Working..."
+node update_custom_status.js "$PENDULUM_STATUS_EMOJI" "$PENDULUM_STATUS_TEXT"
 
 # watch for screen unlock event and update status
 dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
   while read x; do
     case "$x" in
-      *"boolean false"*) node update_custom_status.js "computer" "Working...";
+      *"boolean false"*) node update_custom_status.js "$PENDULUM_STATUS_EMOJI" "$PENDULUM_STATUS_TEXT"
     esac
   done
